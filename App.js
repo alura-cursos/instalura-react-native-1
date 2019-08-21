ff43d9cba98b1f998a1be39aa5ea6ff12b13ffda
Alura <alura@alura.test>
2019-08-21 12:03:54 -0300

    Final Aula 1

---
+++ App.js
@@ -0,0 +1,23 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Fragment} from 'react';
+import {
+  Text
+} from "react-native";
+
+const App = () => {
+  return (
+    <Fragment>
+      <Text>Ricardo</Text>
+      <Text>Marina</Text>
+    </Fragment>
+  )
+};
+
+export default App;
