f548186b11c50bf6fee24c90d992912ef5cbf1c2
Alura <alura@alura.test>
2019-08-23 12:53:00 -0300

    video 5.3

--- src/Components/Comentarios/Comentarios.js
+++ src/Components/Comentarios/Comentarios.js
@@ -4,19 +4,58 @@ import {
     Text,
     TouchableOpacity,
     View,
-    FlatList
+    FlatList,
+    TextInput
 } from "react-native";
 
 import estilo from './estilo';
 
+
 const Comentarios = ({ comentarios }) => {
+    const [estComentarios, setComentarios] = useState(comentarios);
+
+
+    const adicionarComentario = () => {
+        console.warn(conteudoCampoInput);
+        campoInput.clear();
+        const novoComentario = {
+            date: Date.now(),
+            text: conteudoCampoInput,
+            userName: "Bugan"
+        }
+
+        setComentarios([...estComentarios, novoComentario]);
+    }
+
+
+    let campoInput;
+    let conteudoCampoInput = "";
     return (
-        <FlatList
-            data={comentarios}
-            keyExtractor={(item, index) => index.toString()}
-            renderItem={({ item }) =>
-                <Text>{item.text}</Text>}
-        />
+        <Fragment>
+
+            <FlatList
+                data={estComentarios}
+                keyExtractor={(item, index) => index.toString()}
+                renderItem={({ item }) =>
+                <View style={estilo.naMesmaLinha}>
+                    <Text>{item.userName} </Text>
+                    <Text> {item.text}</Text>
+                </View>
+                }
+            />
+            <View style={estilo.naMesmaLinha}>
+                <TextInput
+                    ref={textInput => campoInput = textInput}
+                    onChangeText={texto => conteudoCampoInput = texto}
+                    placeholder={"Deixe seu comentário..."}
+                    style={{ flex: 1 }}
+                />
+                <TouchableOpacity onPress={adicionarComentario}>
+                    <Image source={require("../../../res/img/send.png")}
+                        style={estilo.imgSend} />
+                </TouchableOpacity>
+            </View>
+        </Fragment>
     );
 };
 
---
+++ src/Components/Comentarios/estilo.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+const estilo = StyleSheet.create({
+    naMesmaLinha:{
+        flexDirection:"row",
+        alignItems:"center"
+    },
+    imgSend:{
+        width:30,
+        height:30,
+        marginRight:5
+    }
+})
+
+export default estilo;
